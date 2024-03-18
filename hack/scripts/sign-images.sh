2aad9ab6bec03dcccf466bb7e350845a31b1e4d6
Noel Georgi <git@frezbo.dev>
2024-03-18 20:10:33 +0530

    fix: image signing script
    An overlay might support multiple SBC's from a single image. No point in
    trying to sign an already signed image. So select images to sign unique
    by image name.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- hack/scripts/sign-images.sh
+++ hack/scripts/sign-images.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-EXTERNAL_OVERLAY_IMAGES=$(crane export "${OVERLAYS_IMAGE_REF}" | tar x --to-stdout overlays.yaml | yq '.overlays[] | .image + "@" + .digest')
+EXTERNAL_OVERLAY_IMAGES=$(crane export "${OVERLAYS_IMAGE_REF}" | tar x --to-stdout overlays.yaml | yq '.overlays | unique_by(.image) | .[] | .image + "@" + .digest')
 OVERLAY_IMAGE_DIGEST=$(crane digest "${OVERLAYS_IMAGE_REF}")
 
 for IMAGE in ${EXTERNAL_OVERLAY_IMAGES} ${OVERLAYS_IMAGE_REF}@${OVERLAY_IMAGE_DIGEST}; do
